5f5e0799e19d7f39dee96e7322d5fd021835dc5b
Daryl Olson <daryl.olson@anova.com>
2021-09-18 15:24:09 -0600

    Lesson 2: Creating a Nuxt App

---
+++ pages/create.vue
@@ -0,0 +1,5 @@
+﻿<template>
+  <div>
+    <h1>Create An Event</h1>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Events</h1>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
